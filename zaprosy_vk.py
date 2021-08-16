e1a2f94dafaaec368b87629ee78a17601860a311
afilashka <afilashka@mail.ru>
2021-08-16 11:36:12 +0500

    #4 переделала функцию под запрос по одному id

--- zaprosy_vk.py
+++ zaprosy_vk.py
@@ -6,24 +6,24 @@ import setting
 
 def get_profiles(user_id, access_token):
     url = 'https://api.vk.com/method/users.get?user_ids={}&fields={}&access_token={}&v=5.80'
-    ids = ','.join(str(user) for user in user_id)
-    fields = ['first_name', 'last_name', 'city']
+    #ids = ','.join(str(user) for user in user_id) пока сделаем для одного юзера
+    fields = ['first_name', 'last_name', 'sex', 'city', 'bdate', 'education']
     fd = ','.join(fields)
 
     json_response = requests.get(url.format(ids, fd, access_token)).json()
 
     if json_response.get('error'):
-        print(json_response.get('error'))
-        return None
+        #print(json_response.get('error'))
+        return 'такого аккаунта нет, либо закрытый аккаунт'
     users_d = json_response['response']
     df = pandas.DataFrame(users_d)
-    return df
+    a = df.iloc[0, :].to_string(header=False, index=True)
+    return a
 
 
-ids = [141536612, 139046077, 137464025, 109024405, 203137367, 191107635]
+#ids = 15631
 
-users_d = get_profiles(ids, setting.token_vk_api)
+#users_d = get_profiles(ids, setting.token_vk_api)
 
-a = users_d.iloc[0,:].to_string(header=False, index=True)
-print(a)
+#print(users_d)
 #df.to_csv('file1.csv', encoding='utf-8')
