ec8e0680246bebf88b633aca3fbd2c64dff12fbc
afilashka <afilashka@mail.ru>
2021-08-16 12:39:51 +0500

    #8

--- groups_get.py
+++ groups_get.py
@@ -7,23 +7,21 @@ import json
 import requests
 import time
 
-def groups_search(key_word, gr_amount, token):
+def groups_search(key_word, count, token):
+    url = 'https://api.vk.com/method/groups.search?q={}&sort=2&count={}&access_token={}&v=5.80'
+    col_names =  ['id', 'name', 'screen_name', 'is_closed', 'type','photo_50' , 'photo_100', 'photo_200']
+    my_df1  = pd.DataFrame(columns = col_names)
 
-    url = 'https://api.vk.com/method/groups.search?q={}&city_id={}&sort=2&access_token={}&v=5.80'
-    i = 0
-col_names =  ['id', 'name', 'screen_name', 'is_closed', 'type','photo_50' , 'photo_100', 'photo_200']
-my_df1  = pd.DataFrame(columns = col_names)
-
-for key in key_words:
-    time.sleep(1)
-    json_response = requests.get(url.format(key, city_id, token)).json()
-    i = i + 1
-    if json_response.get('error'):
-        print(json_response.get('error'))
-        print('error in step '&i)
-    else:
-        a = json_response['response']
-        b = a['items']
+    for key in key_words:
+        time.sleep(1)
+        json_response = requests.get(url.format(key, city_id, token)).json()
+        i = i + 1
+        if json_response.get('error'):
+            print(json_response.get('error'))
+            print('error in step '&i)
+        else:
+            a = json_response['response']
+        b    = a['items']
         d = a['count']
         if d != 0:
             c = pd.DataFrame(b)
